f6f6273cc50b0ac7bc25ea97fb469ed43b346886
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-20 10:57:44 +0300

    m,

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + '../client/build')));
 
 app.get('/api/hello', (req, res) => {
 
   res.send({ express: 'Hello From Express' });
+});
+
+app.get('*', (req, res) =>{
+  res.send(express.static(path.join(__dirname + '../client/build/index.html')))
 });
 
 app.post('/api/world', (req, res) => {
@@ -20,3 +26,5 @@ app.post('/api/world', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = app;
