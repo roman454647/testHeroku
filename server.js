b33e8846290ca2f83822d5bb2f00f2774d3c1355
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-20 11:00:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname + '../client/build')));
+
 
 app.get('/api/hello', (req, res) => {
 
   res.send({ express: 'Hello From Express' });
 });
 
-app.get('*', (req, res) =>{
-  res.send(express.static(path.join(__dirname + '../client/build/index.html')))
-});
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
