bc1e971d1a942c2a9198320de1681a19c21f17f2
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-20 11:48:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.post('/api/world', (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
+
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('/app', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
-}
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
