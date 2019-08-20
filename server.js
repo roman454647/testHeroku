79ff9196b93472ee2d751d788fcbe22a820de37a
roman454647 <37308167+roman454647@users.noreply.github.com>
2019-08-20 10:14:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
 
   res.send({ express: 'Hello From Express' });
 });
